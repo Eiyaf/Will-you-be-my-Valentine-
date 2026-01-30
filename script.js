90ea4f2c382e8aa12e47f33a8b31b38196d91ead
Eiyaf <149339986+Eiyaf@users.noreply.github.com>
2026-01-30 16:15:53 -0500

    gifs

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const gifs = [
+  "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdDJocmt4dWcwaTVxbmM0b3NxYWFhcWF2Y2hhMWg5eTBrdHcwMmF6aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/yPAWyc35MhDUNrzySi/giphy.gif",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWk3MGt2YndnbzNqZmVxZHJwZWFmZ3RjODRjNGoyOWswZ245eWpvciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/1tiwQhwReXrSQ66wsB/giphy.gif",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXcxY2VzdG9oeDJva282ZWl1ZXdjdHEyd2RraGJkMno2MDRkYmozeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/754u3UNlbbc2tMg97K/giphy.gif",
+  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHYyNzY5NzU3M3lnNG5kamllODZjcTlxbDNyMnZzNjNlZm1qeHBhZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/EADTT88AK7YUNY7qaq/giphy.gif",
+  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGV0NGJ1bXIxemswa2ZtZmRoN3lkNWw3cnR1bjRuamRoNmxtMXp6cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/JCSr1AN4mNgLKAy3a0/giphy.gif",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjZzM256ZG9oeWQzOHYyazFneTg4YWQ3ZHA3N3M5MnZ3d2lxaTlqNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/wYzNWKJ2jU4rNX6SAO/giphy.gif",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGJ1cHM2azF6ZXo3N2Z5MGphZXdzaGNtNWRiM2lsdWFzMW5pdm45MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/wNk71OcVtOPncxNHVT/giphy.gif",
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWVkajkxazV5eXFwdm5lMjRnbzQ1amc4cndvYzRqcW5vMm9hOW0yOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/fjxupFeOlzwRnhKT0b/giphy.gif",
+];
+
+// Pick a random GIF when the page loads
+function setRandomGif() {
+  const img = document.getElementById("valentine-gif");
+  if (img) {
+    const randomIndex = Math.floor(Math.random() * gifs.length);
+    img.src = gifs[randomIndex];
+  }
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", setRandomGif);
+} else {
+  setRandomGif();
+}
+
 const messages = [
   "Are you sure?",
   "Really sure??",
